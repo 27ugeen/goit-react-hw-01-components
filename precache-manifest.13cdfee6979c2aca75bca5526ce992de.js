605dad54fc3a6e41e6123629edcbc85bb50a40ca
27ugeen <27ugeen@gmail.com>
2020-01-21 01:45:18 +0200

    Updates

---
+++ precache-manifest.13cdfee6979c2aca75bca5526ce992de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "337355b3d798d93ef5f450f0c7da05d2",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "7def0e2730975e5f39d2",
+    "url": "/goit-react-hw-01-components/static/css/main.54ab5f95.chunk.css"
+  },
+  {
+    "revision": "c9b4603ab169b6a847c3",
+    "url": "/goit-react-hw-01-components/static/js/2.7ed1cf1c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-01-components/static/js/2.7ed1cf1c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7def0e2730975e5f39d2",
+    "url": "/goit-react-hw-01-components/static/js/main.6f8112e6.chunk.js"
+  },
+  {
+    "revision": "5529903743abc23e6f33",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.5c5371af.js"
+  }
+]);
